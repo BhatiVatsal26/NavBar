4f2c34a840a0c132b86c986a247fb2b56d94fc85
Bhati Vatsal <105050661+BhatiVatsal26@users.noreply.github.com>
2024-05-31 17:40:24 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -8,10 +8,19 @@ bar.onclick = function(){
     bar.style.display = "none";
     navulR.style.display = "block";
     logores.style.display = "none";
+    navulR.style.transform = "translateX(0px)";
+    navulR.style.animationName = "responsivenav";
 }
 croos.onclick = function(){
+    navulR.style.animationName = "responsivenavsecond";
+    setTimeout(() => {
+    navulR.style.transform = "translateX(0px)";
     croos.style.display = "none";
     bar.style.display = "block";
     navulR.style.display = "none";
     logores.style.display = "block";
+    }, 1000);
+    setTimeout(() => {
+    navulR.style.transform = "translateX(360px)";
+    }, 1000);
 }
